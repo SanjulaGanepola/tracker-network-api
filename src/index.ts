2253aa02036741c7ee89caa64d56f77b30b7e812
Sanjula Ganepola <sanjulagane@gmail.com>
2024-09-09 00:15:00 -0400

    fix export
    Signed-off-by: Sanjula Ganepola <sanjulagane@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { TrackerNetworkAPI } from "./trackerNetworkAPI";
 export { ApexLegends } from "./games/apexLegends";
 export { Csgo } from "./games/csgo";
-export { Splitgate } from "./games/splitGate";
+export { Splitgate } from "./games/splitgate";
 export { TheDivision2 } from "./games/theDivision2";
